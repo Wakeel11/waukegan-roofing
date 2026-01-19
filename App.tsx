89cc67fcdcf20bd8d0688a71aebdbe732d90a277
Wakeel11 <161950974+Wakeel11@users.noreply.github.com>
2026-01-20 03:28:56 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
-// Components aur Pages ke Imports
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import CityServicePage from './pages/CityServicePage';
@@ -9,17 +8,13 @@ import ServicesPage from './pages/ServicesPage';
 import ContactPage from './pages/ContactPage';
 import AreasWeServe from './pages/AreasWeServe';
 
-const App: React.FC = () => {
+const App = () => {
   return (
     <HashRouter>
       <Routes>
-        {/* Layout ke andar routes taake Header/Footer har jagah nazar aaein */}
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          
-          {/* Dynamic City Route */}
           <Route path="roofing-:citySlug" element={<CityServicePage />} />
-          
           <Route path="services" element={<ServicesPage />} />
           <Route path="areas-we-serve" element={<AreasWeServe />} />
           <Route path="contact" element={<ContactPage />} />
