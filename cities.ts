16d67d209d50bdd9715796994d121a7805489930
Wakeel11 <161950974+Wakeel11@users.noreply.github.com>
2026-01-20 03:03:16 +0500

    Update cities.ts

--- cities.ts
+++ cities.ts
@@ -1,4 +1,4 @@
-export const illinoisCities = [
+import { illinoisCities } from '../cities';
   { name: "Waukegan", slug: "waukegan", zip: "60085" },
   { name: "Gurnee", slug: "gurnee", zip: "60031" },
   { name: "Zion", slug: "zion", zip: "60099" },
@@ -7,4 +7,5 @@ export const illinoisCities = [
   { name: "Beach Park", slug: "beach-park", zip: "60087" },
   { name: "Mundelein", slug: "mundelein", zip: "60060" },
   { name: "Round Lake", slug: "round-lake", zip: "60073" }
+
 ];
