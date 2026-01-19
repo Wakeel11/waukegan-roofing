ed7d011c1c6cd082b0c9032296ee46fdf412896c
Wakeel11 <161950974+Wakeel11@users.noreply.github.com>
2026-01-20 03:26:07 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
+// Components aur Pages ke Imports
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import CityServicePage from './pages/CityServicePage';
+import ServicesPage from './pages/ServicesPage';
+import ContactPage from './pages/ContactPage';
+import AreasWeServe from './pages/AreasWeServe';
+
 const App: React.FC = () => {
   return (
     <HashRouter>
       <Routes>
-        {/* Layout ke andar rakha taake Header/Footer nazar aaein */}
+        {/* Layout ke andar routes taake Header/Footer har jagah nazar aaein */}
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           
-          {/* Ye rahi aap ki Dynamic SEO Route */}
+          {/* Dynamic City Route */}
           <Route path="roofing-:citySlug" element={<CityServicePage />} />
           
           <Route path="services" element={<ServicesPage />} />
