6ff0fd4af8157530a76377b769e663bcbaaaf523
Wakeel11 <161950974+Wakeel11@users.noreply.github.com>
2026-01-20 03:45:44 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
-import Layout from './components/Layout';
 import Home from './pages/Home';
 import CityServicePage from './pages/CityServicePage';
 import ServicesPage from './pages/ServicesPage';
@@ -12,13 +11,11 @@ const App = () => {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/roofing-:citySlug" element={<CityServicePage />} />
-          <Route path="/services" element={<ServicesPage />} />
-          <Route path="/areas-we-serve" element={<AreasWeServe />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/roofing-:citySlug" element={<CityServicePage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/areas-we-serve" element={<AreasWeServe />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </HashRouter>
   );
