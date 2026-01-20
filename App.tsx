3c5818f830a861fd0cd2191c0e610075d205772b
Wakeel11 <161950974+Wakeel11@users.noreply.github.com>
2026-01-20 21:25:15 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import CityServicePage from './pages/CityServicePage';
-import RoofingServices from './pages/RoofingServices'; // Naya Page
-import GutterServices from './pages/GutterServices';   // Naya Page
+import Roofing from './pages/Roofing'; // Match your filename
+import Gutters from './pages/Gutters'; // Match your filename
 import ContactPage from './pages/ContactPage';
 import AreasWeServe from './pages/AreasWeServe';
 
@@ -14,15 +14,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          
-          {/* Alag Alag Service Pages */}
-          <Route path="roofing-services" element={<RoofingServices />} />
-          <Route path="gutter-services" element={<GutterServices />} />
-          
+          <Route path="roofing-services" element={<Roofing />} />
+          <Route path="gutter-services" element={<Gutters />} />
           <Route path="areas-we-serve" element={<AreasWeServe />} />
           <Route path="contact" element={<ContactPage />} />
-          
-          {/* Dynamic City Route */}
           <Route path="roofing-:citySlug" element={<CityServicePage />} />
         </Route>
       </Routes>
