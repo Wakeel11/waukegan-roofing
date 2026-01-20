a18604d15932fa5e0856afbde3e295332173ee2b
Wakeel11 <161950974+Wakeel11@users.noreply.github.com>
2026-01-20 22:25:17 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
-import CityTemplate from './pages/CityTemplate'; // Name check karein
+import CityServicePage from './pages/CityServicePage'; // Filename exactly matches yours
 import Roofing from './pages/Roofing'; 
 import Gutters from './pages/Gutters'; 
 import ContactPage from './pages/ContactPage';
@@ -14,14 +14,17 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          
           <Route path="roofing-services" element={<Roofing />} />
           <Route path="gutter-services" element={<Gutters />} />
+          
           <Route path="services" element={<Navigate to="/roofing-services" replace />} />
+          
           <Route path="areas-we-serve" element={<AreasWeServe />} />
           <Route path="contact" element={<ContactPage />} />
           
-          {/* CRITICAL FIX: :cityName exactly matches CityTemplate */}
-          <Route path="roofing-:cityName" element={<CityTemplate />} />
+          {/* Parameter name ':citySlug' matches CityServicePage params */}
+          <Route path="roofing-:citySlug" element={<CityServicePage />} />
         </Route>
       </Routes>
     </HashRouter>
