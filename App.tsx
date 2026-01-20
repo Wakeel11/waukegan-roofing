7e0250123bde58e9bb0fe421e7b366e52829e9a1
Wakeel11 <161950974+Wakeel11@users.noreply.github.com>
2026-01-20 22:12:19 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
-import CityServicePage from './pages/CityServicePage';
+import CityTemplate from './pages/CityTemplate'; // CityServicePage ki jagah CityTemplate use kar rahe hain
 import Roofing from './pages/Roofing'; 
 import Gutters from './pages/Gutters'; 
 import ContactPage from './pages/ContactPage';
@@ -15,18 +15,21 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           
-          {/* Service Pages */}
+          {/* Main Service Pages */}
           <Route path="roofing-services" element={<Roofing />} />
           <Route path="gutter-services" element={<Gutters />} />
           
-          {/* Redirect old services link */}
+          {/* Redirect old services link to Roofing */}
           <Route path="services" element={<Navigate to="/roofing-services" replace />} />
           
           <Route path="areas-we-serve" element={<AreasWeServe />} />
           <Route path="contact" element={<ContactPage />} />
           
-          {/* Dynamic City Route - MUST match the URL structure */}
-          <Route path="roofing-:citySlug" element={<CityServicePage />} />
+          {/* DYNAMIC CITY ROUTE FIX: 
+            Yahan humne variable ka naam ":cityName" rakha hai 
+            taake ye CityTemplate.tsx ke "useParams" se match kare.
+          */}
+          <Route path="roofing-:cityName" element={<CityTemplate />} />
         </Route>
       </Routes>
     </HashRouter>
