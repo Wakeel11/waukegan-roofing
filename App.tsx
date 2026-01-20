c9c9efce3abca234e9df03271a14c2f08e3ac99d
Wakeel11 <161950974+Wakeel11@users.noreply.github.com>
2026-01-20 18:19:38 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,7 +3,8 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import CityServicePage from './pages/CityServicePage';
-import ServicesPage from './pages/ServicesPage';
+import RoofingServices from './pages/RoofingServices'; // Naya Page
+import GutterServices from './pages/GutterServices';   // Naya Page
 import ContactPage from './pages/ContactPage';
 import AreasWeServe from './pages/AreasWeServe';
 
@@ -13,10 +14,15 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="services" element={<ServicesPage />} />
+          
+          {/* Alag Alag Service Pages */}
+          <Route path="roofing-services" element={<RoofingServices />} />
+          <Route path="gutter-services" element={<GutterServices />} />
+          
           <Route path="areas-we-serve" element={<AreasWeServe />} />
           <Route path="contact" element={<ContactPage />} />
-          {/* Ye line city links ko specific banati hai */}
+          
+          {/* Dynamic City Route */}
           <Route path="roofing-:citySlug" element={<CityServicePage />} />
         </Route>
       </Routes>
